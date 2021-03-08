113bcd78b5a1ed0fc560d1b6610fdef38eb28be5
danfrgo <danielfr_goncalves@hotmail.com>
2021-03-08 12:00:41 +0000

    Criar Models

---
+++ ControloFinanceiro.BLL/Models/Categoria.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ControloFinanceiro.BLL.Models
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }
+        public string Nome { get; set; }
+        public string Icone { get; set; }
+
+        // chave estrangeira para relacionar com a model Tipo
+        public int TipoId { get; set; }
+        public string Tipo{ get; set; }
+
+        // relacionamentos
+        public virtual ICollection<Despesa> Despesas { get; set; }
+        public virtual ICollection<Ganho> Ganhos { get; set; }
+    }
+}
---
+++ ControloFinanceiro.BLL/Models/Tipo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ControloFinanceiro.BLL.Models
+{
+    public class Tipo
+    {
+        public int TipoId { get; set; }
+        public int Nome { get; set; }
+
+        //o Tipo terá n categorias logo preciso de uma coleçao de categorias
+        public virtual ICollection<Categoria> Categorias { get; set; }
+
+    }
+}
