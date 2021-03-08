7bf9c4274d0133b2da4f3c6f244c7ca9a5e22ceb
danfrgo <danielfr_goncalves@hotmail.com>
2021-03-08 16:58:06 +0000

    Contexto e Migrations

---
+++ ControloFinanceiro.DAL/Contexto.cs
@@ -0,0 +1,42 @@
+﻿using ControloFinanceiro.BLL.Models;
+using ControloFinanceiro.DAL.Mapeamentos;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ControloFinanceiro.DAL
+{
+    public class Contexto : IdentityDbContext<Utilizador, Funcao, string>
+    {
+        public DbSet<Cartao> Cartoes { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Despesa> Despesas { get; set; }
+        public DbSet<Funcao> Funcoes { get; set; }
+        public DbSet<Ganho> Ganhos { get; set; }
+        public DbSet<Mes> Meses { get; set; }
+        public DbSet<Tipo> Tipos { get; set; }
+        public DbSet<Utilizador> Utilizadores { get; set; }
+
+        // construtor para herdar da classe
+        public Contexto(DbContextOptions<Contexto> opcoes) : base(opcoes) { }
+
+        // OnModelCreating -> funcao para criar a Base de dados de acordo com os mapeamentoss
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.ApplyConfiguration(new CartaoMap());
+            builder.ApplyConfiguration(new CategoriaMap());
+            builder.ApplyConfiguration(new DespesaMap());
+            builder.ApplyConfiguration(new FuncaoMap());
+            builder.ApplyConfiguration(new GanhoMap());
+            builder.ApplyConfiguration(new MesMap());
+            builder.ApplyConfiguration(new TipoMap());
+            builder.ApplyConfiguration(new UtilizadorMap());;
+        }
+
+    }
+}
+
